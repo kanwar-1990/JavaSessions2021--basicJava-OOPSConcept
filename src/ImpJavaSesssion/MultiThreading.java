273de8c44ef98dda59242346b163f84c354c9338
kanwar-1990 <knwrpl.singh@gmail.com>
2021-08-15 17:35:02 +0530

    MultiThreading

---
+++ src/ImpJavaSesssion/MultiThreading.java
@@ -0,0 +1,47 @@
+package ImpJavaSesssion;
+
+class Dominos extends Thread {
+
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("TodaysOffer-Independance Day offer is 10% off on any Pizza");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+}
+
+class PizzaHut extends Thread {
+
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("TodaysOffer-Independance Day offer is 15% off on any Pizza");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+			}
+
+		}
+	}
+}
+
+public class MultiThreading {
+
+	public static void main(String[] args) // Main in one Thread, Which is provided for executiom
+
+	{
+		Dominos d1 = new Dominos();
+		PizzaHut p1 = new PizzaHut();
+
+		d1.start();
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+		}
+		p1.start();
+
+	}
+
+}
