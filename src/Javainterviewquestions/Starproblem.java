39b8b1a7c801852dd477c401415d42051a2b1660
kanwar-1990 <knwrpl.singh@gmail.com>
2021-09-01 17:01:09 +0530

    star problem

---
+++ src/Javainterviewquestions/Starproblem.java
@@ -0,0 +1,41 @@
+package Javainterviewquestions;
+
+public class Starproblem {
+/*
+code 1
+****
+****
+****	
+****	
+
+code 2
+*
+**
+***
+****
+*****	
+	
+	
+	
+*/	
+	
+	public static void main(String args[]) {
+		for (int j = 0; j <= 5; j++) {
+			for (int i = 0; i <= 5; i++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println("**********************************************");
+
+		for (int f = 0; f <= 5; f++) {
+
+			for (int k = 0; k < f; k++) {
+				System.out.print("*");
+
+			}
+			System.out.println();
+		}
+	
+	}
+}
